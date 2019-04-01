838cbf4edf6ccc1691442c77ade74d7ad5bcce9a
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-01 00:24:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
   
   def add_student(student_name, grade)
     if @roster.has_key?(grade)
-    @roster[grade] << student_name
+      @roster[grade] << student_name
     else
       @roster[grade] = []
       @roster[grade] << student_name
